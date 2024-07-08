ccb6bfae7e6055b2695648067283f092b7d296b7
Daniel Hudson <LuckyLlama72@gmail.com>
2024-07-07 23:50:11 -0400

    Possible patch

--- index.js
+++ index.js
@@ -14,19 +14,52 @@ const questions = [{
     choices: ["Research", "Logistics", "Manufacturing", "Security", "Programming"]
 }]
 
+
 inquirer
-.prompt(questions)
-.then((response) =>
-console.log(response))
-
-const pool = new Pool( 
-    {
-        user:'username',
-        password: 'password',
-        host: 'localhost',
-        database: 'employees_db'
-    },
-    console.log('Connected to the employees_db database!')
-)
+  .prompt({
+    type: "list",
+    message: "Welcome to Hyperion Employee Tracker! Please select an option.",
+    name: "bootupFunction",
+    choices: ["New", "View"]
+  })
+  .then((response) => {
+    executePrompt(response)
+})
 
 
+function executePrompt(response) {
+    console.log(response);
+    if (response == 'New'){
+        inquirer
+        .prompt(questions)
+        .then((response) =>
+        console.log(response))
+     } else if (response == 'View') {
+         console.log("Somebody fix the employee database!")
+     }
+    
+}
+
+
+
+
+// inquirer
+// .prompt(questions)
+// .then((response) =>
+// console.log(response))
+
+// const pool = new Pool( 
+//     {
+//         user:'username',
+//         password: 'password',
+//         host: 'localhost',
+//         database: 'employees_db'
+//     },
+//     console.log('Connected to the employees_db database!')
+// )
+
+// pool.connect();
+
+// pool.query('SELECT * FROM employee_names', function (err, {rows}) {
+//     console.log(rows);
+// })
